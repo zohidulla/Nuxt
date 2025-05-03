09fda977bb0f40bc101196fe93a18ff0a988bf24
zohidulla <zohidjonkarimov@gmail.com>
2025-05-03 23:25:27 +0500

    Module Google Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,35 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ["@/assets/css/main.css"],
+  css: ['@/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
   },
   app: {
     head: {
-      title: "Code Editor",
+      title: 'Code Editor',
     },
   },
-
   modules: [
-    "@nuxt/content",
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-    "@nuxt/icon",
-    "@nuxt/image",
-    "@nuxt/scripts",
-    "@nuxt/test-utils",
-    "@nuxt/ui",
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@nuxtjs/google-fonts',
   ],
-});
+  googleFonts: {
+    display: 'swap',
+    families: {
+      'Open Sans': {
+        wght: '300..800',
+        ital: '300..800',
+      },
+    },
+  },
+})
