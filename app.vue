cbb72fd6c7a833a1ba11bb41cbad831d6d26e093
zohidulla <zohidjonkarimov@gmail.com>
2025-05-04 08:10:39 +0500

    Module icons

--- app.vue
+++ app.vue
@@ -5,18 +5,7 @@
     >
       <div class="container flex items-center justify-between gap-x-10">
         <a href="#" class="flex items-center gap-x-1 sm:gap-x-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-8 -rotate-6 text-indigo-600 sm:size-12 dark:text-white"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm14.25 6a.75.75 0 0 1-.22.53l-2.25 2.25a.75.75 0 1 1-1.06-1.06L15.44 12l-1.72-1.72a.75.75 0 1 1 1.06-1.06l2.25 2.25c.141.14.22.331.22.53Zm-10.28-.53a.75.75 0 0 0 0 1.06l2.25 2.25a.75.75 0 1 0 1.06-1.06L8.56 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-2.25 2.25Z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <Icon name="heroicons:code-bracket-square-solid" class="size-8 -rotate-6 text-indigo-600 sm:size-12 dark:text-white" />
           <span class="text-xl font-black sm:text-2xl dark:text-white"
             >Code Editor</span
           >
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,4 +32,7 @@ export default defineNuxtConfig({
       },
     },
   },
+  icon: {
+    mode: 'svg',
+  },
 })
