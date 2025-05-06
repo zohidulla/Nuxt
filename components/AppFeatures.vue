a4dbd96bde8108fe9f4ad73412fd51bb3e70e8a1
zohidulla <zohidjonkarimov@gmail.com>
2025-05-06 06:43:44 +0500

    Main components

---
+++ components/AppFeatures.vue
@@ -0,0 +1,85 @@
+<template>
+  <section
+        id="features"
+        class="flex flex-col gap-y-8 bg-gray-50 py-8 sm:gap-y-12 sm:py-16 dark:bg-gray-700"
+      >
+        <div class="container max-w-screen-sm">
+          <h2
+            class="text-center text-2xl font-bold sm:text-4xl dark:text-white"
+          >
+            Unlock the Full Potential
+          </h2>
+          <p
+            class="mt-1 text-center text-gray-600 sm:mt-2 sm:text-lg dark:text-gray-300"
+          >
+            With tailored settings, keyboard shortcuts, and powerful extensions,
+            you can work smarter and faster.
+          </p>
+        </div>
+        <div class="container grid gap-4 lg:grid-cols-3 lg:gap-8">
+          <a href="#">
+            <article
+              class="h-full rounded-md border border-gray-200 bg-white p-4 hover:border-indigo-600 sm:rounded-xl sm:p-6 dark:border-black dark:bg-gray-800 dark:hover:border-indigo-400"
+            >
+              <header class="flex flex-col gap-y-3">
+                <Icon
+                  name="cog"
+                  class="size-10 text-indigo-600 sm:size-12 dark:text-indigo-400"
+                />
+                <h3 class="font-semibold sm:text-lg dark:text-white">
+                  Settings
+                </h3>
+              </header>
+              <p
+                class="mt-1 text-sm text-gray-600 sm:text-base dark:text-gray-400"
+              >
+                Personalize your workspace to match your unique coding style and
+                preferences effortlessly.
+              </p>
+            </article>
+          </a>
+          <a href="#">
+            <article
+              class="h-full rounded-md border border-gray-200 bg-white p-4 hover:border-indigo-600 sm:rounded-xl sm:p-6 dark:border-black dark:bg-gray-800 dark:hover:border-indigo-400"
+            >
+              <header class="flex flex-col gap-y-3">
+                <Icon
+                  name="fire"
+                  class="size-10 text-indigo-600 sm:size-12 dark:text-indigo-400"
+                />
+                <h3 class="font-semibold sm:text-lg dark:text-white">
+                  Keyboard Shortcuts
+                </h3>
+              </header>
+              <p
+                class="mt-1 text-sm text-gray-600 sm:text-base dark:text-gray-400"
+              >
+                Streamline your workflow with essential shortcuts that save you
+                time and boost efficiency.
+              </p>
+            </article>
+          </a>
+          <a href="#">
+            <article
+              class="h-full rounded-md border border-gray-200 bg-white p-4 hover:border-indigo-600 sm:rounded-xl sm:p-6 dark:border-black dark:bg-gray-800 dark:hover:border-indigo-400"
+            >
+              <header class="flex flex-col gap-y-3">
+                <Icon
+                  name="puzzle"
+                  class="size-10 text-indigo-600 sm:size-12 dark:text-indigo-400"
+                />
+                <h3 class="font-semibold sm:text-lg dark:text-white">
+                  Extensions
+                </h3>
+              </header>
+              <p
+                class="mt-1 text-sm text-gray-600 sm:text-base dark:text-gray-400"
+              >
+                Expand your capabilities with powerful extensions tailored to
+                enhance your coding experience.
+              </p>
+            </article>
+          </a>
+        </div>
+      </section>
+</template>
---
+++ components/AppHero.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="pt-24 pb-12 sm:pt-16 sm:pb-20 lg:pt-28 lg:pb-48">
+    <div
+      class="container flex flex-col items-center gap-8 sm:gap-12 lg:flex-row"
+    >
+      <div
+        class="flex flex-col items-center justify-center text-center lg:items-start lg:text-left dark:text-white"
+      >
+        <h1 class="text-3xl font-black tracking-tight sm:text-5xl">
+          Your
+          <span
+            class="bg-gradient-to-r from-purple-600 to-indigo-600 bg-clip-text text-transparent dark:from-purple-300 dark:to-indigo-300"
+          >
+            VS Code
+          </span>
+          Boost
+        </h1>
+        <p class="mt-2 text-gray-600 sm:mt-4 sm:text-lg dark:text-gray-200">
+          Unlock your coding potential with tailored settings, shortcuts,
+          and expert tutorials.
+        </p>
+        <div class="mt-6 flex gap-x-2 sm:mt-8">
+          <a
+            href="#"
+            class="rounded-md bg-indigo-600 px-6 py-2 font-semibold text-indigo-50 hover:bg-indigo-700 sm:rounded-lg sm:px-8 sm:py-3 sm:text-lg"
+            >Explore</a
+          >
+          <a
+            href="#features"
+            class="rounded-md bg-gray-100 px-6 py-2 font-semibold text-gray-900 hover:bg-gray-200 sm:rounded-lg sm:px-8 sm:py-3 sm:text-lg"
+          >
+            Learn More
+          </a>
+        </div>
+      </div>
+      <div
+        class="relative scale-90 lg:w-2/3 lg:scale-110 lg:perspective-distant"
+      >
+        <div class="hero-reflection" />
+        <img src="/editor.png" alt="Editor UI" class="hero-image" >
+      </div>
+    </div>
+  </section>
+</template>
