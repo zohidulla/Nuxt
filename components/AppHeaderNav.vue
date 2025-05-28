b7a90202fa3e9e105e6100a868cbcab147138522
zohidulla <zohidjonkarimov@gmail.com>
2025-05-28 09:14:05 +0500

    Refactor AppHeaderNav to use AppHeaderNavItem for improved reusability and maintainability

--- components/AppHeaderNav.vue
+++ components/AppHeaderNav.vue
@@ -1,29 +1,12 @@
 <template>
   <nav class="hidden grow sm:block">
     <ul class="flex items-center gap-x-8">
-      <li class="mr-auto">
-        <a
-          href="#"
-          class="font-semibold hover:text-indigo-600 dark:text-gray-200 dark:hover:text-indigo-200"
-          >Library</a
-        >
-      </li>
-      <li>
-        <a
-          href="#"
-          class="font-semibold hover:text-indigo-600 dark:text-gray-200 dark:hover:text-indigo-200"
-        >
-          Log In
-        </a>
-      </li>
-      <li>
-        <a
-          href="#"
-          class="rounded-md bg-indigo-600 px-3 py-2 font-semibold text-indigo-50 hover:bg-indigo-700"
-        >
-          Sign Up
-        </a>
-      </li>
+      <AppHeaderNavItem class="mr-auto">Library</AppHeaderNavItem>
+      <AppHeaderNavItem>Log In</AppHeaderNavItem>
+      <AppHeaderNavItem
+        link-classes="rounded-md bg-indigo-600 px-3 py-2 font-semibold text-indigo-50 hover:bg-indigo-700">
+        Sign Up
+      </AppHeaderNavItem>
     </ul>
   </nav>
 </template>
---
+++ components/AppHeaderNavItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const {
+  linkClasses = 'font-semibold hover:text-indigo-600 dark:text-gray-200 dark:hover:text-indigo-200',
+} = defineProps<{ linkClasses?: string }>()
+</script>
+
+<template>
+  <li>
+    <a href="#" :class="linkClasses">
+      <slot />
+    </a>
+  </li>
+</template>
