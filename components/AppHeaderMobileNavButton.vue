b318d4fc4c21d5c49a428c2f82027790b3e2a54d
zohidulla <zohidjonkarimov@gmail.com>
2025-05-29 09:06:18 +0500

    Add AppHeaderMobileNavItem and AppHeaderMobileNavButton components for improved modularity and reusability

---
+++ components/AppHeaderMobileNavButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  icon: string
+}>()
+</script>
+
+<template>
+  <a href="#nav" class="-mr-1.5 rounded-md p-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+    <Icon name="bars" class="size-6 dark:text-white" />
+  </a>
+</template>
---
+++ components/AppHeaderMobileNavItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const {
+  linkClasses = 'inline-block w-full rounded-md p-2 text-center text-sm font-semibold hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700',
+} = defineProps<{ linkClasses?: string }>()
+</script>
+
+<template>
+  <li class="w-full">
+    <a href="#" :class="linkClasses">
+      <slot />
+    </a>
+  </li>
+</template>
