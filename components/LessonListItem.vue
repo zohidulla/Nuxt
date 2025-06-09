318378abfff01734d877055dc08938a98dd4a76f
zohidulla <zohidjonkarimov@gmail.com>
2025-06-09 09:01:06 +0500

    Refactor LessonListItem component; extract link classes into a computed property for improved readability and maintainability.

--- components/LessonListItem.vue
+++ components/LessonListItem.vue
@@ -5,13 +5,20 @@ const props = defineProps<{
 
 const playlistSlug = useRoute().params.playlistSlug
 const lessonSlug = props.lesson.title.toLowerCase().replaceAll(' ', '-')
+
+const linkClasses = computed(() => [
+  'flex w-full items-center justify-between gap-x-4 rounded-lg p-3 dark:text-gray-300 dark:hover:bg-gray-700',
+  useRoute().params.lessonSlug === lessonSlug
+    ? 'bg-indigo-50 text-indigo-700 dark:bg-gray-700'
+    : 'text-gray-600 hover:bg-gray-100',
+])
 </script>
 
 <template>
   <li>
     <NuxtLink
       :to="`/playlists/${playlistSlug}/lessons/${lessonSlug}`"
-      class="flex w-full items-center justify-between gap-x-4 rounded-lg p-3 text-gray-600 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700"
+      :class="linkClasses"
     >
       <span class="truncate text-sm font-medium sm:text-base">
         {{ lesson.number }}. {{ lesson.title }}
