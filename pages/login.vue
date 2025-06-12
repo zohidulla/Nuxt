dfb2e47868880faae8b5793a618e04e10eb81d85
zohidulla <zohidjonkarimov@gmail.com>
2025-06-12 09:03:36 +0500

    Refactor authentication layout; restructure auth layout to improve organization and enhance user experience with consistent styling across login and register pages.

--- pages/login.vue
+++ pages/login.vue
@@ -3,67 +3,45 @@ definePageMeta({ layout: 'auth' })
 </script>
 
 <template>
-  <section class="flex w-full flex-col items-center gap-y-4 p-3 sm:gap-y-6">
-    <a href="/" class="flex items-center">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        class="size-14 -rotate-6 text-indigo-600 sm:size-16 dark:text-white"
+  <form class="flex flex-col gap-y-4 sm:gap-y-6">
+    <div class="flex flex-col gap-y-1">
+      <label
+        class="text-sm font-medium text-gray-600 sm:text-base dark:text-gray-300"
+        for="email"
+        >Email</label
       >
-        <path
-          fill-rule="evenodd"
-          d="M3 6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm14.25 6a.75.75 0 0 1-.22.53l-2.25 2.25a.75.75 0 1 1-1.06-1.06L15.44 12l-1.72-1.72a.75.75 0 1 1 1.06-1.06l2.25 2.25c.141.14.22.331.22.53Zm-10.28-.53a.75.75 0 0 0 0 1.06l2.25 2.25a.75.75 0 1 0 1.06-1.06L8.56 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-2.25 2.25Z"
-          clip-rule="evenodd"
-        />
-      </svg>
-    </a>
-    <div
-      class="h-full w-full rounded-md border border-gray-200 bg-white p-4 shadow-sm sm:w-96 sm:rounded-lg sm:p-6 dark:border-black dark:bg-gray-900 dark:shadow-none"
-    >
-      <form class="flex flex-col gap-y-4 sm:gap-y-6">
-        <div class="flex flex-col gap-y-1">
-          <label
-            class="text-sm font-medium text-gray-600 sm:text-base dark:text-gray-300"
-            for="email"
-            >Email</label
-          >
-          <input
-            class="rounded-md border border-gray-300 px-4 py-2 text-sm shadow-sm focus:border-indigo-600 focus:ring-1 sm:rounded-lg sm:text-base dark:border-0 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:ring-indigo-400"
-            type="email"
-            id="email"
-          />
-        </div>
-        <div class="flex flex-col gap-y-1">
-          <label
-            class="text-sm font-medium text-gray-600 sm:text-base dark:text-gray-300"
-            for="password"
-          >
-            Password
-          </label>
-          <input
-            class="rounded-md border border-gray-300 px-4 py-2 text-sm shadow-sm focus:border-indigo-600 focus:ring-1 sm:rounded-lg sm:text-base dark:border-0 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:ring-indigo-400"
-            type="password"
-            id="password"
-          />
-        </div>
-        <button
-          class="mt-2 rounded-md bg-indigo-600 px-6 py-2 text-sm font-semibold text-indigo-50 hover:bg-indigo-700 sm:mt-1 sm:rounded-lg sm:text-base"
-        >
-          Log In
-        </button>
-      </form>
-      <p
-        class="mt-6 text-center text-sm text-gray-600 sm:mt-8 dark:text-gray-300"
-      >
-        Don't have an account?
-        <a
-          href="/register.html"
-          class="font-medium text-indigo-600 hover:text-indigo-700 dark:text-indigo-400 dark:hover:text-indigo-300"
-        >
-          Sign Up
-        </a>
-      </p>
+      <input
+        class="rounded-md border border-gray-300 px-4 py-2 text-sm shadow-sm focus:border-indigo-600 focus:ring-1 sm:rounded-lg sm:text-base dark:border-0 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:ring-indigo-400"
+        type="email"
+        id="email"
+      />
     </div>
-  </section>
+    <div class="flex flex-col gap-y-1">
+      <label
+        class="text-sm font-medium text-gray-600 sm:text-base dark:text-gray-300"
+        for="password"
+      >
+        Password
+      </label>
+      <input
+        class="rounded-md border border-gray-300 px-4 py-2 text-sm shadow-sm focus:border-indigo-600 focus:ring-1 sm:rounded-lg sm:text-base dark:border-0 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:ring-indigo-400"
+        type="password"
+        id="password"
+      />
+    </div>
+    <button
+      class="mt-2 rounded-md bg-indigo-600 px-6 py-2 text-sm font-semibold text-indigo-50 hover:bg-indigo-700 sm:mt-1 sm:rounded-lg sm:text-base"
+    >
+      Log In
+    </button>
+  </form>
+  <p class="mt-6 text-center text-sm text-gray-600 sm:mt-8 dark:text-gray-300">
+    Don't have an account?
+    <NuxtLink
+      to="/register"
+      class="font-medium text-indigo-600 hover:text-indigo-700 dark:text-indigo-400 dark:hover:text-indigo-300"
+    >
+      Sign Up
+    </NuxtLink>
+  </p>
 </template>
--- ‎layouts/auth.vue
+++ ‎layouts/auth.vue
@@ -2,6 +2,18 @@
   <main
     class="dark grid min-h-screen place-items-center bg-gray-50 dark:bg-gray-800"
   >
-    <slot />
+    <section class="flex w-full flex-col items-center gap-y-4 p-3 sm:gap-y-6">
+      <NuxtLink to="/">
+        <Icon
+          name="code"
+          class="size-14 -rotate-6 text-indigo-600 sm:size-16 dark:text-white"
+        />
+      </NuxtLink>
+      <div
+        class="w-full rounded-md border border-gray-200 bg-white p-4 shadow-sm sm:w-96 sm:rounded-lg sm:p-6 dark:border-black dark:bg-gray-900 dark:shadow-none"
+      >
+        <slot />
+      </div>
+    </section>
   </main>
 </template>
